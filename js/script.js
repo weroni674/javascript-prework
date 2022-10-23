f6b4c7a425798e7ad6e0bb8d5b3545c1c2eddbd6
Weronika Malinowska <weronika.malinowska36@gmail.com>
2022-10-23 19:04:09 +0200

    add logic if else

--- js/script.js
+++ js/script.js
@@ -38,3 +38,39 @@ else(playerInput == '3'){
 
 printMessage('Twój ruch to: ' + playerMove);
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Komputer wygrywa!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Komputer wygrywa!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Komputer wygrywa!');
+}
+
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+else
+  printMessage('Nieznany ruch')
