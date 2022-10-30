4efa7460c44537c8047876c7317b8e392009f0d8
Weronika Malinowska <weronika.malinowska36@gmail.com>
2022-10-30 17:44:44 +0100

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function getMoveName(argMoveId){
+function playGame(playerInput){
+  function getMoveName(argMoveId){
   if(argMoveId == 1){
   return 'kamień';
   }
@@ -13,10 +14,10 @@ function getMoveName(argMoveId){
 
   printMessage('Nie znam ruchu o id ' + argMoveId + '.');
   return 'nieznany ruch';
-}
+  }
 
-function displayResult(computerMove, playerMove){
-  printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
+  function displayResult(computerMove, playerMove){
+    printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
 
   if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
@@ -54,21 +55,47 @@ function displayResult(computerMove, playerMove){
   }
   else
     printMessage('Nieznany ruch')
+  }
+
+
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  console.log('Wylosowana liczba to: ' + randomNumber);
+
+  let computerMove = getMoveName(randomNumber);
+
+  /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
+
+  console.log('Gracz wpisał: ' + playerInput);
+
+  let playerMove = getMoveName(playerInput);
+
+  displayResult(computerMove,playerMove);
+
+function buttonClicked(){
+  printMessage('Guzik został kliknięty');
 }
 
+let testButton = document.getElementById('play-paper');
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+testButton.addEventListener('click', buttonClicked);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+function buttonClicked(){
+  printMessage('Guzik został kliknięty');
+}
 
-let computerMove = getMoveName(randomNumber);
+let testButton = document.getElementById('play-rock');
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+testButton.addEventListener('click', buttonClicked);
 
-console.log('Gracz wpisał: ' + playerInput);
+function buttonClicked(){
+  printMessage('Guzik został kliknięty');
+}
 
-let playerMove = getMoveName(playerInput);
+let testButton = document.getElementById('play-scissors');
 
-displayResult(computerMove,playerMove);
+testButton.addEventListener('click', buttonClicked);
+}
+
 
       
